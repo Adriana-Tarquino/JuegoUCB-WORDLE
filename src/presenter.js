b8e17bed814d99cae8ba78b9d02eb795772a9b39
Adriana Tarquino <atarquinocespedes@gmail.com>
2022-05-16 19:30:46 -0400

    La interaz funcionando letras que estan en la posicion incorrecta

--- src/presenter.js
+++ src/presenter.js
@@ -6,6 +6,7 @@ const div = document.querySelector("#resultado-div");
 const jugar = new Juego();
 
 const div2 = document.querySelector("#letras-div");
+const div3 = document.querySelector("#letrasPoscorrrecta-div")
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -16,5 +17,7 @@ form.addEventListener("submit", (event) => {
     ///Mostrar Letras correctas.
 
     div2.innerHTML = "<p>" + "Letras que estan en palabra en la posición correcta: "+ jugar.comparaLetrasCorrectas(palabraAdivinar,codigo_imput.value) + "</p>";
- }); 
+    div3.innerHTML = "<p>" + "Letras que estan en la palabra pero en posición incorrecta: " + jugar.comparaLetrasInCorrectas(palabraAdivinar,codigo_imput.value) + "</p>";
+
+}); 
   
