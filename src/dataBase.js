bae2fb6e2a33be2f91ae72d4ed6f8a76586424f3
Fabio Copa Mejia <64265042+fabi-cm@users.noreply.github.com>
2022-05-29 20:29:07 -0400

    Se agregaron funciones nuevass

--- src/dataBase.js
+++ src/dataBase.js
@@ -66,9 +66,50 @@ const eliminar = async (actualPalabra) => {
 };
 
 // mostrar();
-// crear();
+// crear("manzana","frutas");
 // actualizar("cantar", "jugar", "verbos");
 // eliminar("caminar");
 
-//una palabra aletoria de una categoria(nombreCategoria) => return una palabra de la categoria 
-//una palabra tiene que estar en la lista de palabras (palabra) => true of false 
+//una palabra aletoria de una categoria(nombreCategoria) => return una palabra de la categoria
+const aleatorioPalabras = async (categoria) => {
+  const palabras = await palabraModel.find(
+    { categoria: categoria },
+    { _id: 0, palabra: 1 }
+  );
+  const lp = [];
+  for (let i = 0; i < palabras.length; i++) {
+    // console.log(palabras[i].palabra);
+    lp.push(palabras[i].palabra);
+  }
+  console.log(lp);
+  var rand = Math.floor(Math.random() * lp.length);
+  var resp = lp[rand];
+  // console.log(res)
+  return resp;
+};
+
+// Prueba de palabra aleatoria
+// aleatorioPalabras("verbos").then((v) => {
+//   console.log(v);
+// });
+
+//una palabra tiene que estar en la lista de palabras (palabra) => true of false
+const verificarPalabra = async (palabraVereficar) => {
+  const palabras = await palabraModel.find(
+    { },
+    { _id: 0, palabra: 1 }
+  );
+  let resp = false;
+  for (let i = 0; i < palabras.length; i++) {
+    // console.log(palabras[i].palabra);
+    if (palabras[i].palabra === palabraVereficar) {
+      resp = true;
+    }
+  }
+  return resp;
+};
+
+// Prueba de verificar si la palabra esta en la base de datos 
+// verificarPalabra("macaco").then((v) => {
+//   console.log(v);
+// });
